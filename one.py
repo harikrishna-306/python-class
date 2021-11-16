c8d12e501b00f87a150c8d77ae7375e56bf40868
harikrishna-306 <hari.kristapati999@gmail.com>
2021-11-16 15:44:04 +0530

    hello

---
+++ one.py
@@ -0,0 +1,18 @@
+e_id =101
+e_name ="Ravi"
+rating = 4.3
+size = [38,40,42]
+size1 = (38,40,42)
+size2 = {38,40,42}
+emp = {'name':"Ravi", 'sal':45000}
+x = range(0,10)
+discount = True
+print(type(discount))
+print(type(e_id))
+print(type(e_name))
+print(type(rating))
+print(type(size))
+print(type(size1))
+print(type(size2))
+print(type(emp))
+print(type(x))
---
+++ two.py
@@ -0,0 +1,8 @@
+size = [38,40,42,44]
+b = bytearray(size)      #commands are write in python are used to asu symbol only 
+
+print(type(b))           #python are everything object only
+
+size1 = {38,40,40}
+fs=frozenset(size1)
+print(type(fs))
